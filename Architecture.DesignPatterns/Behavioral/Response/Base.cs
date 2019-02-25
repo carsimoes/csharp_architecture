a2c32b22702f0458c55c91ccf0ed0c676c6041df
tonare <carlos.renato.simoes@gmail.com>
2019-02-25 11:45:23 -0300

    Response

---
+++ Architecture.DesignPatterns/Behavioral/Response/Base.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Architecture.DesignPatterns.Behavioral.Response
+{
+	public abstract class Base
+	{
+		public bool Success { get; protected set; }
+
+		public string DescriptionMessage { get; protected set; }
+
+		public Exception InternalException { get; protected set; }
+
+		public Base(bool success, string descriptionMessage, Exception internalException)
+		{
+			Success = success;
+			DescriptionMessage = descriptionMessage;
+			InternalException = internalException;
+		}
+	}
+}
---
+++ Architecture.DesignPatterns/Behavioral/Response/Response.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Architecture.DesignPatterns.Behavioral.Response
+{
+	public class Response<T> : Base
+	{
+		public T Entity { get; private set; }
+
+		public Response(bool success = true, string descriptionMessage = "", Exception internalException = null) : base(success, descriptionMessage, internalException)
+		{
+		}
+
+		public Response(bool success, string descriptionMessage, T entity, Exception internalException = null) : base(success, descriptionMessage, internalException)
+		{
+			Entity = entity;
+		}
+	}
+}
